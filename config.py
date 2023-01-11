b6da761e7cb69081dc5ecdd02c81510453aa3b40
GeekyMovies <121683432+GeekyMovies@users.noreply.github.com>
2023-01-11 21:44:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
     BANNED_USERS = []
     DOWNLOAD_LOCATION = "./DOWNLOADS" 
-    API_ID = int(os.environ.get("API_ID", "23311160"))
+    API_ID = int(os.environ.get("API_ID", 23311160))
     API_HASH = os.environ.get("API_HASH" "2a1366013eca4256bce853346dbcda49")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5914875548:AAF9MzN2Mk8QQhkalABpz_45MjQjBnQUOqg")
     OWNER_ID = int(os.environ.get("OWNER_ID", "5691018873"))
